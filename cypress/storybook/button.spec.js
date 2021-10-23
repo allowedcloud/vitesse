722182138eecf221fcd79f1ec83a18e6f27d39f3
AllowedCloud <79382276+allowedcloud@users.noreply.github.com>
2021-10-23 19:41:09 -0400

    Create test for Storybook

---
+++ cypress/storybook/button.spec.js
@@ -0,0 +1,17 @@
+const getIframeBody = () => {
+    // get the iframe > document > body
+    return cy.get('iframe[id="storybook-preview-iframe"]')
+        // and retry until the body element is not empty
+        .its('0.contentDocument.body').should('not.be.empty')
+        // wraps “body” DOM element
+        // https://on.cypress.io/wrap
+        .then(cy.wrap);
+}
+describe('Button', () => {
+    before(() => {
+        cy.visit('/');
+    });
+    it('loads primary button with default text', () => {
+        getIframeBody().get('#root').contains('button', 'Button');
+    });
+});
