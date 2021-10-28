8a77e3eb2a484a19ccf5976c13157b2b25f2fc96
AllowedCloud <79382276+allowedcloud@users.noreply.github.com>
2021-10-28 17:46:56 -0400

    Remove old comment

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -6,18 +6,6 @@ module.exports = {
     "../src/**/*.stories.mdx",
     "../src/**/*.stories.@(js|jsx|ts|tsx)",
   ],
-  // addons: [
-  //   // '@storybook/addon-links',
-  //   // '@storybook/addon-essentials',
-  //   // {
-  //   //   name: '@storybook/addon-postcss',
-  //   //   options: {
-  //   //     postcssLoaderOptions: {
-  //   //       implementation: require('postcss')
-  //   //     }
-  //   //   }
-  //   // }
-  // ],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials'
